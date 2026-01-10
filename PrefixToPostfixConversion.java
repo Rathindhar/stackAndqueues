237a1e35ec59e45a887470a0071f81e638b035c8
Rathindhar <rathindhar2004@gmail.com>
2026-01-10 22:06:44 +0530

    Prefix To PostFix Conversion

---
+++ PrefixToPostfixConversion.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class PrefixToPostfixConversion {
+    public static void main(String[] args) {
+        String str = "/-AB*+DEF";
+        Stack<String> st = new Stack<>();
+        int i=str.length()-1;
+        while(i>=0){
+            char ch = str.charAt(i);
+            if((ch >='a' && ch<='z')||(ch >='A' && ch<='Z')||(ch >='0' && ch<='9')){
+                st.push(ch+"");
+            }
+            else{
+                String top1 = st.pop();
+                String top2 = st.pop();
+                st.push(top1+top2+ch);
+            }
+            i--;
+        }
+        System.out.println(st.peek());
+    }
+}
